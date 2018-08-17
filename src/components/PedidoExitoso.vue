16adcf9f4be6c3c0d730ea7af6512334511160ce
h3dx0 <rob3rto@openmailbox.org>
2018-08-17 07:35:23 -0500

    cambio msg de pedido exitoso

--- src/components/PedidoExitoso.vue
+++ src/components/PedidoExitoso.vue
@@ -2,8 +2,10 @@
 	<div class="row">
 		<div class="alert alert-success alert-dismissible">
 			Gracias por comprar con nosotros. 
-			Cualquier duda o información no dude en contactarnos al
-			<strong>993-160-5514</strong>
+			Cualquier duda o información no dude en contactarnos.
+			Si llamas de un número fijo marca 045-9931-605-514.
+			Si llamas de tu móvil marca 
+			<strong>993-160-5514</strong> o búscanos en Whatsapp.
 		</div>
 	</div>
 </template>
